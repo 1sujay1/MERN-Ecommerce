9b25652ea32da5ea31f5e0cc8c8c6e06f44bd22b
ANPABS1053 <ANPABS1053@anpabs.com>
2022-01-06 23:31:26 +0530

    home page splitted into smaller component and cards  ui done

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './menu-item.style.scss'
+
+const MenuItem = ({ title, imgUrl, size }) => {
+    return (
+        <div className={`menu-item ${size}`} >
+            <div className="background-image"
+                style={{
+                    backgroundImage: `url(${imgUrl})`
+                }} />
+            <div className="content" >
+                <h1 className="title">{title}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    )
+}
+export default MenuItem;
