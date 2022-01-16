6da5e44eb046f7907521300ed230c40afe0eccc3
ANPABS1053 <ANPABS1053@anpabs.com>
2022-01-17 02:07:06 +0530

    separated client and server apps for stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51KGptrSJWXiAb9UttKlO3b7K5eh1lwlapq9hdBLQVfRGVo42JoZNgmeXyJxCXBGNCONg3Hg8XDHEzovLuqEDvXdg00JV1oDMEg'
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successfull')
+        // console.log(token);
+
+        axios({
+            url: 'http://localhost:5000/payment',
+            method: 'post',
+            data: {
+                token
+            }
+        }).then(res => {
+            // console.log("res", res);
+            alert("Payment successfull")
+        }).catch(err => {
+            console.log("Error", err);
+            console.log("Parsed Error", JSON.parse(err));
+            alert("Please make sure to use provided credit card")
+        })
+
     }
 
     return (
@@ -22,6 +38,7 @@ const StripeCheckoutButton = ({ price }) => {
             panelLabel="Pay now"
             token={onToken}
             stripeKey={publishableKey}
+            allowRememberMe
         />
     )
 }
